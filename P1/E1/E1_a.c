01e198a2e97e4bcf843597e19abc16b542564973
MarcosRigal <riveragavilanmarcos@gmail.com>
2020-09-26 18:15:14 +0200

    Done E1

--- P1/E1/E1_a.c
+++ P1/E1/E1_a.c
@@ -1,11 +1,31 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdio.h>
 
 int main()
 {
-  int n;
+  int n,status;
   printf("Introduzca el número de procesos hijo que desea generar: ");
   scanf("%d", &n);
-  printf("%d\n",n);
-  
+  printf("Soy %d el padre de todos\n", getpid());
+  for (int i = 0; i < n; i++)
+  {
+    switch(fork())
+    {
+      case 0:
+        printf("Soy %d el hijo nº %d del proceso: %d\n", getpid(), (i+1), getppid());
+        return 0;
+
+      case -1:
+        printf("Error al crear el proceso hijo\n");
+        return -1;
+
+      default:
+        printf("Esperando a que acabe mi hijo nº %d\n", i+1);
+        wait(&status);
+        printf("Ya ha acabado mi hijo con el codigo de salida: %d\n", WEXITSTATUS(status));
+    }
+  }
   return 0;
 }
--- P1/E1/E1_b.c
+++ P1/E1/E1_b.c
@@ -1,10 +1,32 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdio.h>
 
 int main()
 {
-  int n;
+  int n, child, status;
   printf("Introduzca el número de procesos hijo que desea generar: ");
   scanf("%d", &n);
-  printf("%d\n",n);
-  return 0;
+  printf("Soy %d el padre de todos\n", getpid());
+  for (int i = 0; i < n; i++)
+  {
+    child = fork();
+    switch(child)
+    {
+      case 0:
+        printf("Soy %d el hijo del proceso: %d\n", getpid(), getppid());
+        break;
+
+      case -1:
+        printf("Error al crear el proceso hijo\n");
+        return -1;
+
+      default:
+        printf("Esperando a que acabe mi hijo %d\n", child);
+        wait(&status);
+        printf("Ya ha acabado mi hijo %d con el codigo de salida: %d\n", child, WEXITSTATUS(status));
+        return 0;
+    }
+  }
 }
