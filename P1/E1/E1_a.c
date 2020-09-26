a6b5528c0cbcb581289b22c5c1ab1549442f758e
MarcosRigal <riveragavilanmarcos@gmail.com>
2020-09-26 09:52:53 +0200

    Adding Demo

---
+++ P1/E1/E1_a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  printf("Introduzca el número de procesos hijo que desea generar: ");
+  scanf("%d", &n);
+  printf("%d\n",n);
+  
+  return 0;
+}
---
+++ P1/E1/E1_b.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  printf("Introduzca el número de procesos hijo que desea generar: ");
+  scanf("%d", &n);
+  printf("%d\n",n);
+  return 0;
+}
