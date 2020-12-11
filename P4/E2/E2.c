ed44ceb04dd01824315965901a92e94a390984b4
MarcosRigal <riveragavilanmarcos@gmail.com>
2020-12-11 11:50:49 +0100

    Documenting P4

--- P4/E2/E2.c
+++ P4/E2/E2.c
@@ -20,9 +20,9 @@ int main(int argc, char const *argv[])
    }
 
    int id_Memoria;//Variable para almacenar el identificador de memoria compartida
-	int id_Semaforo;//Variable para almacenar el identificador de memoria compartida
+	int id_Semaforo;//Variable para almacenar el identificador de memoria compartida del semaforo
    int *contador = NULL;//Puntero a la zona de memoria compartida
-	sem_t *mutex = NULL;//Semaforo que bloquea el acceso a la sección crítica
+	sem_t *mutex = NULL;//Puntero a la zona de memoria compartida del semaforo
    int value;//Almacena el valor devuelto por shmdt al realizar la desconexión
 
    key_t Clave = ftok("E2.c",3); //Usamos ftok para generar la clave de acceso a la zona de memoria compartida
@@ -57,7 +57,7 @@ int main(int argc, char const *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	mutex = (sem_t *)shmat(id_Semaforo, NULL, 0);
+	mutex = (sem_t *)shmat(id_Semaforo, NULL, 0);//Enganchamos el segmento con el proceso
 	if (mutex == NULL)
 	{
 		printf("Main() de demo1... No consigo enlace a la memoria compartida.\n");
@@ -66,49 +66,49 @@ int main(int argc, char const *argv[])
 	}
 
 	sem_init(mutex, 1, 1);//Inicializamos el semaforo que controla el acceso a la zona crítica
-  int status; //Creamos la variable que almacena el número de hijos y la que almacena el estado de salida del hijo
-  pid_t pid, childpid; //Estas variables almacenan el id de los procesos hijos. 
-  //Pid almacena el valor devuelto al padre tras el fork y chilpid el valor devuelto al padre por la función wait cuando termina de esperar al hijo 
-  printf("Soy %d el padre de todos\n", getpid()); //El proceso padre imprime su id
-  for (int i = 0; i < atoi(argv[1]); i++) //Cuando hacemos el fork la variable i es distinta en cada caso 
-  {//Se crean bucles diferentes e independientes
-    pid = fork(); // Aqúi el proceso tiene su hijo. En el padre pid valdrá el id del hijo y en el hijo pid valdrá 0
-    switch(pid) //En base al valor de pid cada proceso ejecutará una función
-    {
-      case 0: //El fork se ha realizado corractamente
-        for (int i = 0; i < 1000000; i++)
-        {//No hace falta linkearlo ya que con el fork hereda el puntero contador del padre
-           sem_wait(mutex);
-			  printf("Soy %d el hijo nº del proceso: %d\n", getpid(), getppid()); //El hijo se identifica 
-           printf("El valor de la variable es: %d\n", *contador);//Imprime e incrementa el valor de la variable de la zona compartida
-           ++*contador;
-			  sem_post(mutex);
-        }
-        value = shmdt ((char *)contador);
-	     if (value == -1)//Comprobamos el valor devuelto por shmdt al realizar la desconexión
-	     {
-	     	printf("Error en shmdt...\n");
-	     	printf("Valor de errno=%d, definido como %s\n", errno, strerror(errno));
-	     	exit(EXIT_FAILURE);
-	     }
-		  value = shmdt ((sem_t *)mutex);
-	     if (value == -1)//Comprobamos el valor devuelto por shmdt al realizar la desconexión
-	     {
-	     	printf("Error en shmdt...\n");
-	     	printf("Valor de errno=%d, definido como %s\n", errno, strerror(errno));
-	     	exit(EXIT_FAILURE);
-	     }
-        exit(EXIT_SUCCESS); //El hijo muere
+  	int status; //Creamos la variable que almacena el número de hijos y la que almacena el estado de salida del hijo
+  	pid_t pid, childpid; //Estas variables almacenan el id de los procesos hijos. 
+  	//Pid almacena el valor devuelto al padre tras el fork y chilpid el valor devuelto al padre por la función wait cuando termina de esperar al hijo 
+  	printf("Soy %d el padre de todos\n", getpid()); //El proceso padre imprime su id
+  	for (int i = 0; i < atoi(argv[1]); i++) //Cuando hacemos el fork la variable i es distinta en cada caso 
+  	{//Se crean bucles diferentes e independientes
+   	pid = fork(); // Aqúi el proceso tiene su hijo. En el padre pid valdrá el id del hijo y en el hijo pid valdrá 0
+   	switch(pid) //En base al valor de pid cada proceso ejecutará una función
+   	{
+   		case 0: //El fork se ha realizado corractamente
+   	   for (int i = 0; i < 1000000; i++)
+   	   {//No hace falta linkearlo ya que con el fork hereda el puntero contador del padre
+         	sem_wait(mutex);
+				printf("Soy %d el hijo nº del proceso: %d\n", getpid(), getppid()); //El hijo se identifica 
+         	printf("El valor de la variable es: %d\n", *contador);//Imprime e incrementa el valor de la variable de la zona compartida
+         	++*contador;
+				sem_post(mutex);
+        	}
+        	value = shmdt ((char *)contador);
+	     	if (value == -1)//Comprobamos el valor devuelto por shmdt al realizar la desconexión
+	     	{
+	     		printf("Error en shmdt...\n");
+	     		printf("Valor de errno=%d, definido como %s\n", errno, strerror(errno));
+	     		exit(EXIT_FAILURE);
+	     	}
+	  		value = shmdt ((sem_t *)mutex);
+	     	if (value == -1)//Comprobamos el valor devuelto por shmdt al realizar la desconexión
+	     	{
+	     		printf("Error en shmdt...\n");
+	     		printf("Valor de errno=%d, definido como %s\n", errno, strerror(errno));
+	     		exit(EXIT_FAILURE);
+	     	}
+        	exit(EXIT_SUCCESS); //El hijo muere
 
-      case -1: //Ha ocurrido un error al realizar el fork
-        printf("Error al crear el proceso hijo\n"); //Se informa al usuario
-        exit(EXIT_FAILURE); //Indica que ha ocurrido un fallo en la ejecución
+      	case -1: //Ha ocurrido un error al realizar el fork
+      		printf("Error al crear el proceso hijo\n"); //Se informa al usuario
+      	 	exit(EXIT_FAILURE); //Indica que ha ocurrido un fallo en la ejecución
 
-      default:
-      printf("Esperando a que acabe mi hijo nº %d\n", i+1);
-    }//Como el padre no ha hecho exit continua con el for y crea otro hijo
-  }//Una vez el padre ha terminado de crear los hijos que le hemos solicitado empieza a esperarlos
-  while ( (childpid=waitpid(-1, &status, WUNTRACED | WCONTINUED)) > 0 )//Si lo hacemos así en vez de con wait podemos saber si el proceso ha sido pausado y poniendo -1 en el primer parametro de waitpid esperamos a cualquier hijo
+      	default:
+      		printf("Esperando a que acabe mi hijo nº %d\n", i+1);
+   	 }//Como el padre no ha hecho exit continua con el for y crea otro hijo
+  	}//Una vez el padre ha terminado de crear los hijos que le hemos solicitado empieza a esperarlos
+  	while ( (childpid=waitpid(-1, &status, WUNTRACED | WCONTINUED)) > 0 )//Si lo hacemos así en vez de con wait podemos saber si el proceso ha sido pausado y poniendo -1 en el primer parametro de waitpid esperamos a cualquier hijo
 	{//Este bucle se repetirá mientas haya hijos que esperar cuando no haya mas wait devolverá -1
 		if (WIFEXITED(status)) 
 		{//Entrará en el caso de que el hijo haya finaizado correctamente ya que WIFEXITED(status) devolverá true
@@ -128,7 +128,7 @@ int main(int argc, char const *argv[])
 		printf("Error en la invocacion de wait o waitpid. Valor de errno = %d, definido como: %s\n", errno, strerror(errno));
 		exit(EXIT_FAILURE); //Acaba el proceso padre con error
 	} 
-  printf("El valor de la variable es: %d\n", *contador);//Imprimimos el valor de la variable
+  	printf("El valor de la variable es: %d\n", *contador);//Imprimimos el valor de la variable
    value = shmctl (id_Memoria, IPC_RMID, (struct shmid_ds *)NULL);//Como no vamos a volver a usar la memoria compartida, la marcamos para borrado.
    if (value == -1)//Comprobamos el valor devuelto por shmctl
 	{
